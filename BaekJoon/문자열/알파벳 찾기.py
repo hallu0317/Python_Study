c2ff4b54feab10ba5de2936cbeccf87671010aec
kimjiwon <hallu0317@gmail.com>
2022-12-21 21:09:30 +0900

    10809

---
+++ BaekJoon/문자열/알파벳 찾기.py
@@ -0,0 +1,10 @@
+str = input()
+list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+for i in range(0,len(list)):
+    print(str.find(list[i]))
+
+# alpha = list(range(97,123))
+#
+# for x in alpha:
+#     print(str.find(chr(x)))
