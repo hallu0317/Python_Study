5e1a05a80af36ac483289b22badc44ca1b510209
kjiwon <hallu0317@gmail.com>
2023-02-02 16:46:46 +0900

    레벨 2_

--- Programmers/Lv2/다음 큰 숫자.py
+++ Programmers/Lv2/다음 큰 숫자.py
@@ -1,13 +1,12 @@
 def solution(n):
     answer = 0
     b = format(n, 'b')
-    tmp2 = b.count('1')
 
     temp = 0
     while True:
         n = n + 1
         b2 = format(n, 'b')
-        if b2.count('1') == tmp2:
+        if b2.count('1') == b.count('1'):
             temp = b2
             break
 
